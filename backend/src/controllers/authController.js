8343503c4a7f5e795e1104b0f88e97ea7cf31956
minhtri2404 <triho24042003@gmail.com>
2025-10-16 22:52:10 +0700

    git commit -m "feat(auth): hoàn thành chức năng đăng nhập backend"

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -45,6 +45,53 @@ class AuthController{
            return res.status(500).json({success: false, error: error.message})
         }
     }
+
+    // Chức năng dăng nhập
+    login = async(req, res) => {
+        try {
+            const {email, password} = req.body
+            
+            // Kiểm tra xem email có tồn tại hay không
+            const user = await User.findOne({ email})
+            if (!user) {
+                return res.status(400).json({success: false, message: "Email không tồn tại"})
+            }
+
+            // So sánh mật khẩu
+            const isMatch = await bcrypt.compare(password, user.password)
+            if (!isMatch) {
+                return res.status(404).json({success: false, message: "Sai mật khẩu"})
+            }
+
+             // Tạo JWT
+            const token = jwt.sign(
+                {id: user._id, role: user.role},
+                process.env.JWT_SECRET,
+                {expiresIn: process.env.JWT_EXPIRATION}
+            )
+
+            // Gửi token qua cookie
+            res.cookie('token', token, {
+                httpOnly: true, // không truy cập được bằng JS (bảo mật)
+                secure: process.env.NODE_ENV === 'production', // chỉ bật HTTPS trong production
+                sameSite: 'strict', // chống CSRF
+                maxAge: 3600000, // 1h (tùy chỉnh theo JWT_EXPIRATION)
+            })
+
+            return res.status(200).json({
+                success: true,
+                message: "Đăng nhập thành công",
+                user: {
+                    id: user._id, 
+                    name: user.name, 
+                    role: user.role
+                }
+            })
+
+        } catch (error) {
+            return res.status(500).json({success: false, error: error.message})
+        }
+    }
 }
 
 module.exports = new AuthController()
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,6 +4,6 @@ const authController = require('../controllers/authController')
 const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/register', authController.register)
-
+router.post('/login', authController.login)
 
 module.exports = router;
