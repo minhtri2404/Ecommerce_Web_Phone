f2379ab152511b19188d41540cadf246bb865290
minhtri2404 <triho24042003@gmail.com>
2025-10-22 23:09:09 +0700

    Cài đặt plugin admin

---
+++ admin/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container text-center mt-5">
+    <h1 class="text-3xl font-bold underline">🎉 Vue 3 + Vite </h1>
+  </div>
+</template>
+
+<script setup>
+// Không cần logic ở đây
+</script>
